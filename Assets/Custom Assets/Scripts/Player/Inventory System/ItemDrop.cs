3858c7b093a1d6a172030ac9cca4f0decf115eed
AlucardVergil <44729950+AlucardVergil@users.noreply.github.com>
2023-05-20 22:35:29 +0300

    Created custom editor based on ItemDrop itemType enum value

--- Assets/Custom Assets/Scripts/Player/Inventory System/ItemDrop.cs
+++ Assets/Custom Assets/Scripts/Player/Inventory System/ItemDrop.cs
@@ -20,10 +20,7 @@ public class ItemDrop : MonoBehaviour
     public int maxStack;
     public List<InventoryItem> ingredients; // List of items required to craft this item
     public GameObject itemDropPrefab;
-
-    // Additional properties specific to certain item types
-    public int attackPower; // For weapons
-    public int defensePower; // For armor
+        
     public bool isConsumable; // For consumable items
 
     // Enum to define the item type
@@ -37,6 +34,23 @@ public class ItemDrop : MonoBehaviour
 
     public UnityEvent onConsumeItem;
 
+
+    // Additional properties specific to certain item types
+    #region Equipment Variables
+    [HideInInspector] public int attackPower; // For weapons
+    [HideInInspector] public int defensePower; // For armor
+    #endregion
+
+    #region Consumable Variables
+    [HideInInspector] public int effectsDropdownIndex = 0;
+    [HideInInspector] public int effectsValue = 0;
+    #endregion
+
+    #region Resources Variables
+    #endregion
+
+    #region Craftable Variables
+    #endregion
 
 
     private void Start()
---
+++ Assets/Editor/ItemDropEditor.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+//Set this editor in the inspector as the editor for ItemDrop class. So when i add the ItemDrop script as
+//a component of an object it runs and displays this editor
+[CustomEditor(typeof(ItemDrop))] 
+public class ItemDropEditor : Editor
+{
+    private ItemDrop itemDrop;
+
+    
+
+    //Function that overrides the OnInspectorGUI() from the base Editor class that ItemDropEditor inherits
+    public override void OnInspectorGUI()
+    {
+        //target derives from Editor class and is the current object that is being inspected, meaning the selected ItemDrop object
+        itemDrop = (ItemDrop)target;
+
+        base.OnInspectorGUI();
+
+        switch (itemDrop.itemType)
+        {
+            case ItemDrop.ItemType.Resource: 
+                GUI_Resource();
+                break;
+            case ItemDrop.ItemType.Consumable:
+                GUI_Consumable();
+                break;
+            case ItemDrop.ItemType.Equipment: 
+                GUI_Equipment();
+                break;
+            case ItemDrop.ItemType.Craftable:
+                GUI_Craftable();
+                break;
+        }
+
+    }
+
+
+    private void GUI_Consumable()
+    {
+        string[] effects = new string[]
+        {
+            "Restores Health",
+            "Restores Hunger",
+            "Restores Thirst"
+        };
+
+        itemDrop.effectsDropdownIndex = EditorGUILayout.Popup("Consumable Effect", itemDrop.effectsDropdownIndex, effects);
+        itemDrop.effectsValue = EditorGUILayout.IntField("Restoration Effect Value", itemDrop.effectsValue);
+
+    }
+
+
+
+    private void GUI_Resource()
+    {
+        
+    }
+
+
+    private void GUI_Equipment()
+    {
+        itemDrop.attackPower = EditorGUILayout.IntField("Attack Power", itemDrop.attackPower);
+        itemDrop.defensePower = EditorGUILayout.IntField("Defense Power", itemDrop.defensePower);
+    }
+
+
+    private void GUI_Craftable()
+    {
+
+    }
+
+
+
+} 
+
